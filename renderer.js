ecbcaea2d0b8bdff2a8285d1c58cd526e52fdd54
atiabbz <atiab.bz@gmail.com>
2022-02-07 11:42:09 +0800

    just skeleton (doesn't work atm)

--- renderer.js
+++
@@ -1,7 +0,0 @@
-fetch("./settings.json")
-  .then((response) => response.json())
-  .then((settings) => {
-    const textarea = document.querySelector("textarea");
-    textarea.style.fontFamily = settings.fontFamily;
-    textarea.style.fontSize = settings.fontSize + "px";
-  });
---
+++ src/renderer/renderer.js
@@ -0,0 +1,9 @@
+// fetch("./settings.json")
+//   .then((response) => response.json())
+//   .then((settings) => {
+//     const textarea = document.querySelector("textarea");
+//     textarea.style.fontFamily = settings.fontFamily;
+//     textarea.style.fontSize = settings.fontSize + "px";
+//   });
+
+console.log("renderer.js >>> Hello!");
